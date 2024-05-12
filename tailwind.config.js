b2fc1ff0f4e7404d30c205b08ed8cfd8c8b6be3e
reynaldo0 <reynaldooyusellino@gmail.com>
2024-05-12 10:29:42 +0700

    update project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: 'Gilda Display',
+      secondary: 'Barlow',
+      tertiary: 'Barlow Condensed',
+    },
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1140px',
+    },
+    extend: {
+      colors: {
+        primary: '#0a0a0a',
+        accent: {
+          DEFAULT: '#a37d4c',
+          hover: '#967142',
+        },
+      },
+      backgroundImage: {
+        room: "url('./assets/img/room.jpg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
